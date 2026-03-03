32d5a5394eac9e9cfc620299e9bb941955670e3e
https://github.com/Devsonar19/My-Notes.git <devsonar19@outlook.com>
2026-03-03 23:36:24 +0530

    a static report card printer

---
+++ report_card_printer.py
@@ -0,0 +1,25 @@
+# REPORT CARD PRINTER 
+
+name = 'Alice'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score,type(score))
+
+'''
+Output:
+
+Alice <class 'str'>
+True <class 'bool'>
+20 <class 'int'>
+True
+80.5 <class 'float'>
+
+'''
